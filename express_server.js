8d0a9e1db08b942dfbd8ec38378b0cb6c2226f22
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-11-02 23:18:17 -0400

    creat shortening url function

--- express_server.js
+++ express_server.js
@@ -6,11 +6,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.use(express.urlencoded({ extended: true })); //encoding the submitted urls in new form to be readable
+const generateRandomString = function() { 
+return Math.random().toString(36).substr(6);
+};
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -21,19 +23,26 @@ app.get("/hello", (req, res) => {
   const templateVars = {greeting: "Hello World"};
   res.render("hello_world", templateVars);
 });
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
  app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
